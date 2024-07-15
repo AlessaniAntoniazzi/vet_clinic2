3872bebe5b51b8b2ddf7dc35015e3c7856373849
Alessani Antoniazzi <alessaniantoniazzi@gmail.com>
2024-07-15 19:50:25 -0300

    feat: add tutor routes

---
+++ src/routes/tutorRoutes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import getTutorControllers from '../controllers/getTutorController';
+import postTutorController from '../controllers/postTutorController';
+import putTutorController from '../controllers/putTutorController';
+import deleteTutorController from '../controllers/deleteTutorController';
+
+const routerTutors = Router();
+routerTutors.get('/tutors', getTutorControllers.getTutors);
+routerTutors.post('/tutors', postTutorController.createTutor);
+routerTutors.put('/tutors/:id', putTutorController.tutorUpdate);
+routerTutors.delete('/tutors/:id', deleteTutorController.deleteTutor);
+
+export { routerTutors };
